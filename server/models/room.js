17699ff747fd0c7e15a45270cc4990ce8840212e
Ismail Celebi <icelebi@k2m17s02.42kocaeli.com.tr>
2025-08-04 02:22:12 +0300

    tables completed

--- server/models/room.js
+++ server/models/room.js
@@ -1,7 +1,30 @@
 const mongoose = require('mongoose')
 
-const roomSchema = new mongoose.Schema([
+const roomSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    maxPeople: {
+        type: Number,
+        required: true,
+    },
+    roomNumbers: [{
+        number: number,
+        unavailableDates : {
+            type: [Date]
+        }
+    }]
 
-])
+
+}, {timestamps: true})
 
 module.exports = mongoose.Schema('Room', roomSchema)
